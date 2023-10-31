efee7a2d04d23f0be4518b4c5eb45fc69b484c99
Elias Xu <flam0799@gmail.com>
2023-10-30 21:27:34 -0400

    initial commit

---
+++ PythonCrashCourse/PCC.1/example.py
@@ -0,0 +1,25 @@
+#this line is a comment
+
+#the next line is a print statement:
+print(0)
+
+#the next line is commented out
+#print(123)
+
+#this is a function we will talk about it later
+def foo():
+    print(1)
+
+print(2)
+
+def foo2():
+    print(3)
+
+print(4)
+foo2()
+
+
+
+
+
+
